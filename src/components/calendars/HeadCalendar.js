5e7629826b6f4b1178fe0b93aa5e8f5869f73b8b
Stive627 <fossistive627@gmail.com>
2025-02-03 19:28:13 +0530

    last update

--- src/components/calendars/HeadCalendar.js
+++ src/components/calendars/HeadCalendar.js
@@ -8,13 +8,13 @@ function HeadCalendar({curMonth, currYear, handleNext, handlePrevious, handleSel
     const [show, setShow] = useState(false)
   return (
     <div className=' w-full py-2 my-1 flex justify-between'>
-        <div className=' flex flex-row'>
-            <div><button className='text-[26px]' onClick={()=> handlePrevious()}><ArrowBackIosNewIcon style={{color:bluecolor}}/></button></div>
-            <div className=' w-32'><p className=' text-center' style={{color:bluecolor, fontWeight:'bold', fontSize:'26px'}}>{ curMonth}</p></div>
-            <div><button className='text-[26px]'  onClick={()=> handleNext()}><ArrowForwardIosIcon style={{color:bluecolor}}/></button></div>
+        <div className=' flex flex-row text-[18px]'>
+            <div><button  onClick={()=> handlePrevious()}><ArrowBackIosNewIcon style={{color:bluecolor}}/></button></div>
+            <div className=' w-24'><p className=' text-center' style={{color:bluecolor, fontWeight:'bold'}}>{ curMonth}</p></div>
+            <div><button   onClick={()=> handleNext()}><ArrowForwardIosIcon style={{color:bluecolor}}/></button></div>
         </div>
         <div className=' relative'>
-            <button onClick={()=> setShow(!show)} style={{color:darkcolor}} className='text-[26px] cursor-pointer'>{currYear}</button>
+            <button onClick={()=> setShow(!show)} style={{color:darkcolor}} className='text-[18px] cursor-pointer'>{currYear}</button>
             { show &&  <div className=' absolute  w-32 bg-white border'>
                           <div className=' w-full flex flex-col divide-y divide-blue-400'>
                             {arr.map((elt, indx) => <button onClick={()=> {handleSelect(indx); setShow(!show);}} key={indx} className={`w-full text-[18px] p-2 ${parseInt(elt) === parseInt(currYear) && 'bg-blue-500 text-white'}`}>{elt}</button>)}
