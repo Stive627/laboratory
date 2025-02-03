b5243a3d73cea44d99a2b938198d754282698f6f
Stive627 <fossistive627@gmail.com>
2025-02-03 19:39:51 +0530

    final

--- src/components/calendars/Month.js
+++ src/components/calendars/Month.js
@@ -9,7 +9,7 @@ function Month({month, cm}) {
     const Square = ({d}) => <div className='  flex justify-center items-center'><div><button style={(currentday(d) && cm )?{color:'white', backgroundColor:'rgba(22, 124, 255, 1)'}:{backgroundColor:'white'}} className={`${currentday(d) && cm &&  ' text-white'} ${currentday(d) && !cm &&' border'} rounded-full  w-8 h-8  m-2  flex items-center justify-center`}><p>{d}</p></button></div></div>
   return (
     <>   
-      <div className = 'flex justify-between'>{title.map((elt, indx) => <p className=' text-center text-[15px]' style={{color:darkcolor}} key={indx}>{elt}</p>)}</div> 
+      <div className = 'flex justify-between'>{title.map((elt, indx) => <p className='  text-[15px] w-full text-center' style={{color:darkcolor}} key={indx}>{elt}</p>)}</div> 
       <div className = ' border border-gray-200 grid grid-cols-7 divide-y divide-x   divide-gray-200'>
           {month.map((elt, indx) => <Square key={indx} d={elt}/>)}
       </div>
