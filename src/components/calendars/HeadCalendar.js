85f3906194e021a9411bac72c3f62d37b8dc0db0
Stive627 <fossistive627@gmail.com>
2025-02-03 19:38:09 +0530

    align

--- src/components/calendars/HeadCalendar.js
+++ src/components/calendars/HeadCalendar.js
@@ -13,13 +13,13 @@ function HeadCalendar({curMonth, currYear, handleNext, handlePrevious, handleSel
             <div className=' w-24'><p className=' text-center' style={{color:bluecolor, fontWeight:'bold'}}>{ curMonth}</p></div>
             <div><button   onClick={()=> handleNext()}><ArrowForwardIosIcon style={{color:bluecolor}}/></button></div>
         </div>
-        <div className=' relative'>
+        <div >
             <button onClick={()=> setShow(!show)} style={{color:darkcolor}} className='text-[18px] cursor-pointer'>{currYear}</button>
-            { show &&  <div className=' absolute  w-32 bg-white border'>
+            { show && <div className=' relative'> <div className=' absolute right-1/4 w-32 bg-white border'>
                           <div className=' w-full flex flex-col divide-y divide-blue-400'>
-                            {arr.map((elt, indx) => <button onClick={()=> {handleSelect(indx); setShow(!show);}} key={indx} className={`w-full text-[18px] p-2 ${parseInt(elt) === parseInt(currYear) && 'bg-blue-500 text-white'}`}>{elt}</button>)}
+                            {arr.map((elt, indx) => <button onClick={()=> {handleSelect(indx); setShow(!show);}} key={indx} className={`w-full text-[18px] p-1 ${parseInt(elt) === parseInt(currYear) && 'bg-blue-500 text-white'}`}>{elt}</button>)}
                           </div>
-                      </div>}
+                          </div></div>}
         </div>
     </div>
   )
