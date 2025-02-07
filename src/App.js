017c5f856fef5162347e59ac6359029eea498b2c
Stive627 <fossistive627@gmail.com>
2025-02-07 09:57:08 +0530

    07-02-25

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { ProfileProvider } from './logic/ContextProfile';
 import Profile from './Pages/Profile';
-import Calendar from  './components/calendars/Calendar'
+import CharUI from './components/charts/ChartUI'
 function App() {
   return (
     <ProfileProvider>
       <Router>
         <Routes>
-          <Route path='/' element={<Calendar/>}/>
+          <Route path='/' element={<CharUI/>}/>
           <Route path='/profile' element={<Profile/>}/>
         </Routes>
       </Router>
