dc280e7288f63adaff6f08e572e0beeef4e0c398
Stive <fossistive627@gmail.com>
2025-02-17 21:31:09 +0530

    second

--- src/components/VerificationCode/VerificationCode.js
+++ src/components/VerificationCode/VerificationCode.js
@@ -18,7 +18,7 @@ function VerificationCode() {
     }
   return (
     <div className=' w-full flex justify-center mt-8'>
-        <div className={`${large? 'w-1/2' : 'w-full px-6'} flex flex-row gap-3`}>
+        <div className={`${large? 'w-1/2' : 'w-full px-6'} flex flex-row gap-3 justify-center`}>
             {localcode.map((elt, indx) => <Square active={i === indx} value={elt} handleChange={(value) => handleChange(value, indx)} handlekey={(ekey) => handleKey(ekey, localcode[indx])} key={indx}/>)}
         </div>
     </div>
