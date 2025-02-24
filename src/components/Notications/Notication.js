22769a2b2f56506de36f2b601f6cfd6c2c64315e
Stive <fossistive627@gmail.com>
2025-02-24 18:48:27 +0530

    more

--- src/components/Notications/Notication.js
+++ src/components/Notications/Notication.js
@@ -6,7 +6,7 @@ import './notication.css'
 function Notication() {
   return (
         <div className=' absolute right-0 -top-14'>
-            <div className=' w-64 bg-white border border-blue-500 p-2 rounded-md first'>
+            <div className=' w-64 bg-gray-50 cursor-pointer border border-blue-500 p-2 rounded-md first'>
                 <p className = 'text-[18px]'><CheckCircleIcon className=' rounded-full text-blue-600'/>{' '} Password sent</p>
             </div>
     </div>
