e51152cff525ca2bee3f7de186ec0ff613b49a90
Stive627 <fossistive627@gmail.com>
2025-03-01 21:39:35 +0530

    01/03/2025

--- src/components/VerificationCode/VerificationCode.js
+++ src/components/VerificationCode/VerificationCode.js
@@ -6,44 +6,51 @@ function VerificationCode({handleCode}){
     const large = useScreen()
     const [localcode, setLocalcode] = useState(['','','','','',''])
     const [i, setI] = useState(0)
+    const [test, setTest] = useState('')
     const [err, setErr] = useState(false)
     function handleChange(value, index){
-        console.log([...localcode, value])
-        if(localcode.slice(0,5).every(elt => elt)){ 
-            console.log('well done ')
-            if(+[...localcode, value].join('') === 111111){
-                console.log('perfect Stive')
+        localcode[index] = value
+        if(localcode.join('').length === 6){ 
+            if(+localcode.join('') === 111111){
                 setErr(false)
             }
             else{
-                console.log('You did a mistake.')
                 setErr(true)
             }
         }
-        localcode[index] = value
         setLocalcode(localcode)
+        if(i === 5 ){
+            setI(5)
+        }
+        else{ 
         setI(i + 1)
+        }
+        console.log(localcode)
         handleCode(localcode.join(''))
+        console.log(i)
 
     }
-    function handleKey(ekey, value){
-        console.log(i)
-        if(ekey === 'Backspace' && !value){
-            setI(i - 1)
+
+    function handleKey(ekey){
+        if(ekey === 'Backspace' || ekey === 'ArrowLeft'){
+            if(i <=0){
+                setI(0)
+            }
+            else{
+                setI(i - 1)
+            }
         }
-        if(!localcode.slice(0,5).every(elt => elt)){ 
-            setErr(false)
-        }
-        if(localcode.every(elt => elt)){ 
-            setErr(true)
+        if(ekey === 'ArrowRight'){
+            setI(i + 1)
         }
     }
 
   return (
     <div className=' w-full flex justify-center mt-8'>
         <div className={`${large? 'w-1/2' : 'w-full px-6'} flex flex-row gap-3 justify-center`}>
-            {localcode.map((elt, indx) => <Square err={err} active={i === indx} value={elt} handleChange={(value) => handleChange(value, indx)} handlekey={(ekey) => handleKey(ekey, localcode[indx ])} key={indx}/>)}
+            {localcode.map((elt, indx) => <Square err={err} active={i === indx} value={elt} handleChange={(value) => handleChange(value, indx)} handlekey={(ekey) => handleKey(ekey)} key={indx}/>)}
         </div>
+        <input onKeyUp={(e) => console.log(e.key)} size={1} value={test} onChange={(e) => setTest(e.target.value.slice(0,1))} className=' border w-full'/>
     </div>
   )
 }
